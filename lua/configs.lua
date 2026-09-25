3132a2e19ed740baae777bfea9cfc4390b949fd8
skjeeth1 <skjeeth1@gmail.com>
2026-09-25 23:29:32 +0530

    Config backup 25/09/2026

--- lua/configs.lua
+++ lua/configs.lua
@@ -1,6 +1,5 @@
 local opt = vim.opt
 
--- opt.colorcolumn = "80" -- Highlight column 80
 opt.signcolumn = "yes"   -- Always show sign column
 opt.termguicolors = true -- Enable true colors
 opt.ignorecase = true    -- Ignore case in search
@@ -8,11 +7,11 @@ opt.smartcase = true
 opt.swapfile = false     -- Disable swap files
 opt.autoindent = true    -- Enable auto indentation
 
-opt.expandtab = false    -- Use spaces instead of tabs
--- opt.tabstop = 4          -- Number of spaces for a tab
--- opt.softtabstop = 4      -- Number of spaces for a tab when editing
-opt.shiftwidth = 4    -- Number of spaces for autoindent
-opt.shiftround = true -- Round indent to multiple of shiftwidth
+opt.expandtab = true     -- Use spaces instead of tabs
+opt.tabstop = 4          -- Number of spaces for a tab
+opt.softtabstop = 4      -- Number of spaces for a tab when editing
+opt.shiftwidth = 4       -- Number of spaces for autoindent
+opt.shiftround = true    -- Round indent to multiple of shiftwidth
 opt.smartindent = true
 opt.smarttab = true
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,6 +12,7 @@ vim.pack.add({
 	{ src = 'https://github.com/stevearc/conform.nvim' },
 	{ src = 'https://github.com/mfussenegger/nvim-lint' },
 	'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+	'https://github.com/lewis6991/gitsigns.nvim',
 	-- { src = 'https://github.com/folke/which-key.nvim' },
 })
 
